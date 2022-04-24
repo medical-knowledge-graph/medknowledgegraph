caf2d32cf4a376c636e98e414daf4daa89e5acd3
Ron Burgundy <r.burgundy96@protonmail.com>
2022-04-24 22:12:39 +0200

    pymedgraph.parse: bug fix

--- pymedgraph/dataextraction/parser.py
+++ pymedgraph/dataextraction/parser.py
@@ -1,28 +1,34 @@
 def get_pubmed_id(paper: dict, sep: str = '~') -> str:
     if 'PubmedData' not in paper.keys():
         raise RuntimeError('Got unexpected Pubmed paper dict to retrieve id from:', paper)
+    indx = 0
     # get list
     id_list = paper.get('PubmedData').get('ArticleIdList')
     # check
     if not id_list:
         raise RuntimeError('Failed to get pubmed id from paper:', paper)
-    if len(id_list) > 1:
-        # TODO: make sure to get pubmed id
-        print('WARNING: Found multiple Ids for paper. Might be ignored.', id_list)
-    if id_list[0].attributes['IdType'] != 'pubmed':
-        print('WARNING: Unexpected `IdType` in IdList. Expected is `pubmed` but found `{it_}`.'.format(
-            it_=id_list[0].attributes['IdType']
-        ))
-
+    # if multiple ids exist make sure to get pubmed
+    if len(id_list) > 1 and id_list[indx].attributes['IdType'] != 'pubmed':
+        for ix, id_ in id_list:
+            if id_ == 'pubmed':
+                indx = ix
+                break
+        if id_list[indx].attributes['IdType'] != 'pubmed':
+            raise RuntimeError(f'Cannot find `pubmed` id in id_list: {id_list}')
     # build uri
-    paper_id = id_list[0].attributes['IdType'] + sep + id_list[0]
+    paper_id = id_list[indx].attributes['IdType'] + sep + id_list[indx]
 
     return paper_id
 
 def parse_pubmed_article(paper: dict) -> str:
     """ get article and join different sections of abstract to one """
-    abstract_sections = paper['MedlineCitation']['Article']['Abstract']['AbstractText']
-    return ' '.join(section.title() for section in abstract_sections)
+    try:
+        abstract_sections = paper['MedlineCitation']['Article']['Abstract']['AbstractText']
+        abstract = ' '.join(section.title() for section in abstract_sections)
+    except KeyError:
+        print('WARNING: Found no Abstract in ', paper['MedlineCitation'])
+        abstract = ''
+    return abstract
 
 
 def get_pubmed_title(paper: dict) -> str:
