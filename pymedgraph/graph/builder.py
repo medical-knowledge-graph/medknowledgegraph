f5a2d62d1371690051d08d91350469bd32fde8bc
Ron Burgundy <r.burgundy96@protonmail.com>
2022-05-03 16:59:48 +0200

    pymedgraph.graph: batch upload to neo4j

--- pymedgraph/graph/builder.py
+++ pymedgraph/graph/builder.py
@@ -1,28 +1,65 @@
+import time
+import pandas as pd
 from neo4j import GraphDatabase
 
 
 class Neo4jBuilder(object):
+    """
+    Using batch upload instead of session + for loop
+    https://towardsdatascience.com/create-a-graph-database-in-neo4j-using-python-4172d40f89c4
+    """
+
     def __init__(self, uri, user, password):
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def add_paper(self, paper: dict, batch_size: int = 1000):
+        paper_uris = list()
+        titles = list()
+        for paper_id, paper_val in paper.items():
+            paper_uris.append(paper_id)
+            titles.append(paper_val['title'])
+        df = pd.DataFrame({'paper_uri': paper_uris, 'title': titles})
+        query = '''
+                    UNWIND $rows AS row
+                    CREATE (p:Paper {uri: row.paper_uri, title: row.title})
+                    RETURN count(*) as total
+                '''
+        return self.insert_data(query, df, batch_size)
+
+    def insert_data(self, query, rows, batch_size=2000):
+        total = 0
+        batch = 0
+        start = time.time()
+        result = None
+
+        while batch * batch_size < len(rows):
+            res = self.query(
+                query,
+                parameters={'rows': rows[batch * batch_size:(batch + 1) * batch_size].to_dict('records')})
+            total += res[0]['total']
+            batch += 1
+            result = {"total": total,
+                      "batches": batch,
+                      "time": time.time() - start}
+            print(result)
+
+        return result
+    def query(self, query, parameters):
+        """make actual query"""
+        session = None
+        response = None
+        try:
+            session = self.driver.session()
+            response = list(session.run(query, parameters))
+        except Exception as ex:
+            print('Query failed:', ex)
+        finally:
+            if session:
+                session.close()
+        return response
 
     def close(self):
         # Don't forget to close the driver connection when you are finished with it
         self.driver.close()
 
-    def create_paper(self, paper):
-        with self.driver.session() as session:
-            for paper_uri, paper_dict in paper.items():
-                result = session.write_transaction(
-                    self._create_paper, paper_uri, paper_dict['title']
-                )
-                for row in result:
-                    print(f'created {row}')
 
-    @staticmethod
-    def _create_paper(tx, paper_id, paper_title):
-        query = (
-            "CREATE (p:Paper {uri: $paper_id, title: $paper_title})"
-            "RETURN p"
-        )
-        result = tx.run(query, paper_id=paper_id, paper_title=paper_title)
-        return [{"p": row["p"]["uri"]} for row in result]
