4aa55793a5488add4c1ec29601b85bab1b4334b6
Ron Burgundy <r.burgundy96@protonmail.com>
2022-05-09 14:34:34 +0200

    pymedgraph.graph: build relations for neo4j

--- pymedgraph/graph/builder.py
+++ pymedgraph/graph/builder.py
@@ -12,6 +12,48 @@ class Neo4jBuilder(object):
     def __init__(self, uri, user, password):
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
 
+    @staticmethod
+    def build_entity_relations(result_dict):
+        ids = list()
+        entities = list()
+        for key, value in result_dict.items():
+            ids.append(key)
+            entities.append(value['entities'])
+        df = pd.DataFrame({'uri': ids, 'entities': entities}).explode('entities')
+        # split up tuple (entity-text, entity-label)
+        df['entity_text'], df['entity_label'] = zip(*df['entities'])
+
+        return df
+
+    @staticmethod
+    def build_entity_nodes(df):
+        # combine different entity labels into one list
+        df = df.groupby('entity_text')['entity_label'].unique().apply(list).reset_index()
+        return df
+
+    def add_entities(self, paper: dict, batch_size: int = 1000):
+        entity_relations = self.build_entity_relations(paper)
+        entity_nodes = self.build_entity_nodes(entity_relations)
+
+        # add nodes
+        query = '''
+                    UNWIND $rows AS row
+                    CREATE (e:Entity {text: row.entity_text, labels: row.entity_label})
+                    RETURN count(*) as total
+                '''
+        self.insert_data(query, entity_nodes, batch_size)
+        # add relations
+        query = '''
+                    UNWIND $rows AS row
+                    MATCH (p:Paper), (e:Entity)
+                    WHERE p.uri = row.uri AND e.text = row.entity_text
+                    CREATE (p)-[:CONTAINS]->(e)
+                    RETURN count(*) as total
+                '''
+        self.insert_data(
+            query, entity_relations.drop_duplicates(['uri', 'entity_text']).drop('entities', axis=1), batch_size
+        )
+
     def add_paper(self, paper: dict, batch_size: int = 1000):
         paper_uris = list()
         titles = list()
@@ -44,6 +86,7 @@ class Neo4jBuilder(object):
             print(result)
 
         return result
+
     def query(self, query, parameters):
         """make actual query"""
         session = None
