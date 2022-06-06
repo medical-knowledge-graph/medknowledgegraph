d226e04971ad715a8c0c9b94451e68d601b8f031
IAMspiegel <s.spiegel@uke.de>
2022-06-06 17:45:40 +0200

    for Soenke

--- pymedgraph/dataextraction/uniprotpipe.py
+++ pymedgraph/dataextraction/uniprotpipe.py
@@ -1,8 +1,13 @@
+import pandas as pd
 from pymedgraph.dataextraction.basepipe import BasePipe
 
 class UniProtPipe(BasePipe):
+    """
+    |source |node label  | $attr$Name  | $attr$UniProtID    |
+    |GenXY  | ProteinXY  | geneName     | xyIDxy            |
+    """
     def __init__(self):
-        pass
+        super().__init__(pipe_name='UniProtPipe')
 
     def _run_pipe(self, **kwargs) -> pd.DataFrame:
         # TODO: implemente here
