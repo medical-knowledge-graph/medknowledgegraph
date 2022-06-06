ba3bee93bd0086ca8653b3a086609e2aa25fa756
IAMspiegel <s.spiegel@uke.de>
2022-06-06 17:43:49 +0200

    pymedgraph: moving methods to pymedgraph.dataextraction.parser

--- pymedgraph/dataextraction/__init__.py
+++ pymedgraph/dataextraction/__init__.py
@@ -1,3 +1,2 @@
 from pymedgraph.dataextraction.entityrecognition import MedGraphNER
-from pymedgraph.dataextraction.mesh import get_mash_terms, get_keywords
-from pymedgraph.dataextraction.parser import get_pubmed_id, get_pubmed_title
+from pymedgraph.dataextraction.parser import get_pubmed_id, get_pubmed_title, get_mash_terms, get_keywords
