aa66f8f940b837e0b22a0c5cb068b0f244f76233
IAMspiegel <s.spiegel@uke.de>
2022-06-21 16:23:57 +0200

    pymedgraph.dataextraction: add UniProtPipe to init

--- pymedgraph/dataextraction/__init__.py
+++ pymedgraph/dataextraction/__init__.py
@@ -2,3 +2,4 @@ from pymedgraph.dataextraction.parser import get_pubmed_id, get_pubmed_title, ge
 from pymedgraph.dataextraction.basepipe import StandardPubMedPipe
 from pymedgraph.dataextraction.entityrecognition import NERPipe
 from pymedgraph.dataextraction.medgen import MedGenPipe
+from pymedgraph.dataextraction.uniprotpipe import UniProtPipe
